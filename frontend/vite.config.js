80e5eb2fed4d03f5a0d0b5890c1be7caf0ee0966
cjlacout-ghb <cjlacout@gmail.com>
2026-05-15 18:27:19 -0300

    Update: 2026-05-15 18:27:19

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
+  build: {
+    outDir: '../electron/frontend_dist'
+  },
   plugins: [react()],
   server: {
     port: 5173,
